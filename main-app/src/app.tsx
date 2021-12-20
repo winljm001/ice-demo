4a253f4adbe5258b64abd867f8658c56caf80cb7
winljm001 <819648983@qq.com>
2021-12-20 10:52:15 +0800

    feat:server init

--- main-app/src/app.tsx
+++ main-app/src/app.tsx
@@ -3,9 +3,24 @@ import { runApp, IAppConfig } from 'ice';
 const appConfig: IAppConfig = {
   app: {
     rootId: 'ice-container',
+    // 可选，自定义添加 Provider
+    // addProvider: ({ children }) => {
+    //   return <ConfigProvider>{children}</ConfigProvider>;
+    // },
+    // 可选，常用于 SSR 场景或者初始化异步获取数据的场景
+    // 如果返回字段中包含 initialStates 字段将会作为状态管理 store 的初始值
+    // 如果返回字段中包含 auth 字段将会作为权限管理 auth 的初始值
+    // getInitialData: async() => {
+    //   const result = await request();
+    //   return result;
+    // },
+
+    ErrorBoundaryFallback: () => {
+      return <div>渲染错误</div>;
+    },
   },
   router: {
-    type: 'hash',
+    type: 'browser',
   },
 };
 
